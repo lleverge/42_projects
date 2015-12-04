5e7ad2c0ef30e20cf4e25401842ebabff8afc130
Livien LEVERGEOIS <lleverge@e1r4p20.42.fr>
2015-12-04 17:07:17 +0100

    enter.c -> verifie que des . ou # dans fichier passe

--- Fillit/enter.c
+++ Fillit/enter.c
@@ -6,27 +6,48 @@
 /*   By: lleverge <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/12/04 16:11:18 by lleverge          #+#    #+#             */
-/*   Updated: 2015/12/04 16:53:50 by lleverge         ###   ########.fr       */
+/*   Updated: 2015/12/04 17:05:57 by lleverge         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 #include <fcntl.h>
 
-int		check_file(char *file_name)
+int			ft_strlen(char *str)
+{
+	int		count;
+
+	count = 0;
+	while (*str)
+	{
+		count++;
+		str++;
+	}
+	return (count);
+}
+
+void		ft_putstr(char *str)
+{
+	write(1, str, ft_strlen(str));
+}
+
+int			check_file(char *file_name)
 {
 	int		fd;
 	char	buf[5];
 	int		ret;
 	int		i;
 
-	fd = open(file_name,  O_RDONLY);
+	fd = open(file_name, O_RDONLY);
 	if (fd == -1)
 		return (1);
 	while ((ret = read(fd, buf, 1)) > 0)
 	{
-		buf[ret] = '\0';
-		printf("%s", buf);
+		if (*buf != '.' && *buf != '#')
+		{
+			ft_putstr("error\n");
+			return (1);
+		}
 	}
 	close(fd);
 	if (close(fd) == -1)
@@ -34,7 +55,7 @@ int		check_file(char *file_name)
 	return (0);
 }
 
-int		main(int argc, char  **argv)
+int			main(int argc, char **argv)
 {
 	if (argc != 2)
 	{
