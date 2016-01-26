909418cb2d3874e340ac46aa210f25e6ba2bf814
Livien LEVERGEOIS <lleverge@e1r4p17.42.fr>
2016-01-26 13:02:25 +0100

    correction GNL, normalement tout fonctionne

--- GNL/get_next_line.c
+++ GNL/get_next_line.c
@@ -5,61 +5,61 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: lleverge <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2016/01/09 11:11:41 by lleverge          #+#    #+#             */
-/*   Updated: 2016/01/18 11:16:38 by lleverge         ###   ########.fr       */
+/*   Created: 2016/01/26 12:58:21 by lleverge          #+#    #+#             */
+/*   Updated: 2016/01/26 13:01:44 by lleverge         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-static char			*str_concat(char *str, char *buff)
+static char		*ft_mem_concat(char *str, char *buffer)
 {
-	char *temp;
+	char *tmp;
 
-	temp = ft_strjoin(str, buff);
+	tmp = ft_strjoin(str, buffer);
 	ft_strdel(&str);
-	return (temp);
+	return (tmp);
 }
 
-static int			get_line(char **line, char *buff, char *overflow)
+static int		ft_get_line(char **line, char *buffer, char *overflow)
 {
-	char *temp;
+	char *tmp;
 
-	if ((temp = ft_strchr(buff, '\n')))
+	if ((tmp = ft_strchr(buffer, '\n')))
 	{
-		*temp = '\0';
-		*line = ft_strdup(buff);
-		ft_memmove(overflow, temp + 1, ft_strlen(temp + 1) + 1);
+		*tmp = '\0';
+		*line = ft_strdup(buffer);
+		ft_memmove(overflow, tmp + 1, ft_strlen(tmp + 1) + 1);
 		return (1);
 	}
 	return (0);
 }
 
-int					get_next_line(int const fd, char **line)
+int				get_next_line(int const fd, char **line)
 {
-	static char overflow[255][BUFF_SIZE + 1];
+	static char overflow[256][BUFF_SIZE + 1];
 	char		buffer[BUFF_SIZE + 1];
 	char		*str;
 	int			ret;
 
-	if (fd < 0)
+	if (fd < 0 || fd > 256 || !line || BUFF_SIZE <= 0)
 		return (-1);
-	if (get_line(line, overflow[fd], overflow[fd]) == 1)
+	if (ft_get_line(line, overflow[fd], overflow[fd]) == 1)
 		return (1);
 	str = ft_strdup(overflow[fd]);
+	ft_memset(overflow[fd], 0, BUFF_SIZE + 1);
 	ft_bzero(buffer, BUFF_SIZE + 1);
 	while ((ret = read(fd, buffer, BUFF_SIZE)) != 0)
 	{
 		if (ret == -1)
 			return (-1);
 		buffer[ret] = '\0';
-		str = str_concat(str, buffer);
-		if (get_line(line, str, overflow[fd]) == 1)
+		str = ft_mem_concat(str, buffer);
+		if (ft_get_line(line, str, overflow[fd]) == 1)
 		{
 			ft_strdel(&str);
 			return (1);
 		}
 	}
-	*line = str;
-	return (0);
+	return ((*line = str) && ft_strlen(*line) != 0);
 }
